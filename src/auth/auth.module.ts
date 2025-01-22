995edacd0c747ed00d858e9736b26d6dd75eef1e
Miqueias dos Santos Sousa Assad <mi.queias2010@hotmail.com>
2025-01-21 22:35:44 -0300

    configurando auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
+  imports: [
+    JwtModule.registerAsync({
+      global: true,
+      imports: [],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: +configService.get<number>('JWT_EXPIRATION_TIME'),
+        },
+      }),
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
